e0c09a90e6e1c4a3c80a7d8047ceeb8f0f6248d1
Nick Cianfarra <nicholascian99@gmail.com>
2023-07-16 01:45:20 -0400

    Added FB with CDN

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js';
+import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCjqXLXnVZNfnQu1aSnpP4GXxkfrZY-Ep4",
+    authDomain: "we-are-the-champions-2f47f.firebaseapp.com",
+    databaseURL: "https://we-are-the-champions-2f47f-default-rtdb.firebaseio.com",
+    projectId: "we-are-the-champions-2f47f",
+    storageBucket: "we-are-the-champions-2f47f.appspot.com",
+    messagingSenderId: "519804551298",
+    appId: "1:519804551298:web:9abc3a6c9e793b31dc349b"
+};
+
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const commentsInDB = ref(database, "Comments")
+
+console.log(commentsInDB)
+
+
+
+
+
+
+
+const textArea = document.querySelector('#textarea')
+
+textArea.textContent = ""
+
